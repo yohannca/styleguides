2b407871d8ac00bacb92c586d2e4206851b135c4
Yohann Catherine <hello@yohann.io>
2018-12-02 10:17:45 +0800

    Properly configure vuepress

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Yohann's Styleguide
+
+## Heading 2
+
+### Heading 3
+
+This is just a paragraph.
+
+#### Heading 4
+
+* List item 1
+* List item 2
+* List item **bold**
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,8 +2,9 @@ module.exports = {
     title: 'My Styleguide',
     description: 'My personal styleguide',
     themeConfig: {
-        nav: [
-            { text: 'Home', link: '/' }
-        ]
+        sidebar: [
+          '/',
+          '/guide'
+        ],
     }
 }
