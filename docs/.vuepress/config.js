e6d961353b20e4ae8bf7659e5fcf9d6e07e5fe6d
Yohann Catherine <hello@yohann.io>
2018-12-02 20:06:29 +0800

    Removes sidebar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,10 +1,5 @@
 module.exports = {
     title: 'My Styleguide',
     description: 'My personal styleguide',
-    themeConfig: {
-        sidebar: [
-            '/',
-            ['/php', 'PHP & Laravel']
-        ]
-    }
+    themeConfig: {}
 }
